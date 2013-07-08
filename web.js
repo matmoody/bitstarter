5877c88a1883fb5bdff051de055076bce369133f
Matt Moody <moody47@gmail.com>
2013-07-08 20:54:15 +0000

    Attempt to fix web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync(index.html);
-buffer.toString("utf-8", 0, buffer.length);
+app.get('/', function(request, response) {
+var fs = require('fs');
+var buffer = new buffer();
+response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
