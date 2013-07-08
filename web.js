823f6f7b78de1ca5bf67596177b06858f10d8798
Matt Moody <moody47@gmail.com>
2013-07-08 20:32:10 +0000

    Edit web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 fs.readFileSync(index.html);
-buffer.toString(['utf8'], [0], [buffer.length]);
+buffer.toString("utf-8", 0, buffer.length);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
